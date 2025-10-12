0c9144b592652e4d8701f8629ccc8c14a64b6702
Gilgamesh <georges5757c@gmail.com>
2025-10-12 23:52:56 +0200

    Modularising each exercice

--- td1/Exercice 2/main.c
+++ td1/Exercice 2/main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <time.h>
-#include "fibonacci.h"
+#include "primes.h"
 
-#define N 40   // Fibonacci test case
+#define n 40000  // Primes test case 
 
 int main() {
     clock_t begin_one, begin_two;
@@ -10,18 +10,17 @@ int main() {
     double time_one, time_two;
 
     begin_one = clock();
-    long long int fib_one = fibonacci_recursive(N);
+    int primes_one = prime_brute(n);
     end_one = clock();
 
     begin_two = clock();
-    long long int fib_two = fibonacci_dynamic(N);
+    int primes_two = prime_dynamic(n);
     end_two = clock();
 
     time_one = (double)(end_one -  begin_one) / CLOCKS_PER_SEC;
     time_two = (double)(end_two -  begin_two) / CLOCKS_PER_SEC;
 
-    printf("Fib recursive time: %lf seconds\n ~Result: %lli\nFib dynamic time: %lf seconds\n ~Result: %lli\n\n",
-         time_one, fib_one, time_two, fib_two);
+    printf("Primes recursive time: %lf seconds\n ~Result: %i\nPrimes dynamic time: %lf seconds\n ~Result: %i\n", time_one, primes_one, time_two, primes_two);
 
     return 0;
 }
