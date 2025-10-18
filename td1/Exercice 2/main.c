08fe36a4353ca69e1cf4020b30e94de2f47961f7
Gilgamesh <georges5757c@gmail.com>
2025-10-18 12:43:38 +0200

    Added comments

--- td1/Exercice 2/main.c
+++ td1/Exercice 2/main.c
@@ -5,22 +5,26 @@
 #define n 40000  // Primes test case 
 
 int main() {
-    clock_t begin_one, begin_two;
-    clock_t end_one, end_two;
+    clock_t begin, end;
     double time_one, time_two;
 
-    begin_one = clock();
+    // Brute Force
+    begin = clock();
     int primes_one = prime_brute(n);
-    end_one = clock();
+    end = clock();
 
-    begin_two = clock();
+    time_one = (double)(end -  begin) / CLOCKS_PER_SEC;
+
+    printf("Primes recursive time: %lf seconds\n ~Result: %i\n", time_one, primes_one);
+
+    // Dynamic Programming
+    begin = clock();
     int primes_two = prime_dynamic(n);
-    end_two = clock();
+    end = clock();
 
-    time_one = (double)(end_one -  begin_one) / CLOCKS_PER_SEC;
-    time_two = (double)(end_two -  begin_two) / CLOCKS_PER_SEC;
+    time_two = (double)(end - begin) / CLOCKS_PER_SEC;
 
-    printf("Primes recursive time: %lf seconds\n ~Result: %i\nPrimes dynamic time: %lf seconds\n ~Result: %i\n", time_one, primes_one, time_two, primes_two);
+    printf("Primes dynamic time: %lf seconds\n ~Result: %i\n", time_two, primes_two);
 
     return 0;
 }
